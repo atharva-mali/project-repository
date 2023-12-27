8d67c6c90b7261ed3e297ae10f7e260774d4cd57
Atharva Mali <atharva.mali3081@gmail.com>
2023-12-27 18:39:56 +0530

    Updated Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,29 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 import React from "react";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PostDetails from "./pages/PostDetails";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <div>
-      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/write" element={<CreatePost/>}/>
+        <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
+        <Route exact path="/edit/:id" element={<EditPost/>}/>
+        <Route exact path="/profile/:id" element={<Profile/>}/>
+
+
+
       </Routes>
-      <Footer />
     </div>
   );
 };
